18b96c55bd04e8f0276de3a9d05496d41331bb6c
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-09-24 18:10:59 +0200

    dufårse

---
+++ Webshop/DAL/DALProduct.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+
+
+
+namespace DAL
+{
+    class DALProduct : DAL
+    {
+
+        public List<Product> GetAllProductsBySearch(Product product)
+        {
+            string sql = "Get * from tblProduct WHERE ";
+
+            if (product.productID != null)
+            {
+                if (count.Count > 0)
+                    sql += "'AND ";
+                sql += $"ProductID = {product.productID} ";
+            }
+            if (product.name != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND ";
+                sql += $"ProductName = {product.name} ";
+            }
+            if (product.category != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND ";
+                sql += $"CategoryID = {product.category} ";
+            }
+            if (product.size != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND ";
+                sql += $"SizeID = {product.size} ";
+            }
+            if (product.Color != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND ";
+                sql += $"ColorID = {product.Color} ";
+            }
+            if (product.brand != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND ";
+                sql += $"BrandID = {product.brand} ";
+            }
+            if (product.description != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND ";
+                sql += $"Description = {product.description} ";
+            }
+            if (product.ppu != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND ";
+                sql += $"PricePerUnit = {product.ppu} ";
+            }
+            if (product.unitsInStock != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND ";
+                sql += $"UnitsInStock = {product.unitsInStock} ";
+            }
+            if (product.picture != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND ";
+                sql += $"PictureID = {product.picture} ";
+            }
+            List<Product> products = new List<Product>();
+            var myCommand = new SqlCommand(sql, connection);
+
+            using (var datareader = myCommand.ExecuteReader())
+            {
+                while (datareader.Read())
+                {
+                    Product item = new Product();
+                    item.productID = Convert.ToInt32(datareader["ProductID"]);
+                    item.name = $"{datareader["ProductName"]}";
+                    item.category = $"{datareader["CategoryID"]}";
+                    item.size = $"{datareader["SizeID"]}";
+                    item.Color = $"{datareader["ColorID"]}";
+                    item.brand = $"{datareader["BrandID"]}";
+                    item.description = $"{datareader["Description"]}";
+                    item.ppu = Convert.ToDecimal(datareader["PricePerUnit"]);
+                    item.unitsInStock = Convert.ToInt32(datareader["UnitsInStock"]);
+                    item.picture = $"{datareader["PictureID"]}";
+                    products.Add(item);
+                }
+            }
+            return products;
+        }
+    }
+}
---
+++ Webshop/Webshop/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webshop.Models
+{
+    public class Product
+    {
+        public int? productID { get; set; }
+        public string name { get; set; }
+        public string category { get; set; }
+        public string size { get; set; }
+        public string Color { get; set; }
+
+        public string brand { get; set; }
+        public string description { get; set; }
+        public decimal? ppu { get; set; }
+        public int? unitsInStock { get; set; }
+        public string picture { get; set; }
+    }
+}
