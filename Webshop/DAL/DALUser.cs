f0182d021b6c3e6675ace991c67cb0e3fc7b7821
Katrina R <katrinarosales@live.com>
2016-09-25 02:11:47 +0200

    GetAllUserBySearch
    UpdateUser
    // Osäker på denna metoden?
    
    InsertUser
    // Tror jag är klar med metoden.

--- Webshop/DAL/DALUser.cs
+++ Webshop/DAL/DALUser.cs
@@ -1,12 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Webshop.Models;
 
 namespace DAL
 {
-    class DALUser
+    class DALUser : DAL
     {
+        List<string> count = new List<string>();
+
+        public List<User> GetAllUserBySearch(User user)
+        {
+            string sql =
+                "SELECT " + 
+                "user.Firstname"+
+                "user.Lastname"+
+                "user.Username"+
+                "user.Password"+
+                "user.StreetAdress"+
+                "zipcode.ZipcodeID"+
+                "city.CityID"+
+                "customergroup.CustomergroupID"+
+                "user.Admin"+
+                "FROM tblUser AS user WHERE ";
+             
+            if (user.firstname != null)
+            {
+                 if(count.Count > 0 )
+                    sql += "AND";
+                    sql += $"FirstName = {user.firstname}";
+             
+            }
+            if (user.lastname != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND";
+                sql += $"LastName = {user.lastname}";
+            }
+            if (user.username != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND";
+                sql += $"Username = {user.username}";
+            }
+            if (user.streetAdress != null)
+            {
+                if (count.Count > 0)
+                    sql += "AND";
+                sql += $"StreetAdress = {user.streetAdress}";
+            }
+
+            sql += "INNER JOIN tblZipcode AS zipcode ON user.ZipcodeID = zipcode.ZipcodeID"
+                + "INNER JOIN tblCity AS city ON user.CityID = city.CityID"
+                + "INNER JOIN tblCustomerGroup AS customergroup ON user.CustomergroupID = customergroup.CustomergroupID";
+
+            List<User> users = new List<User>();
+            var myCommand = new SqlCommand(sql, connection);
+
+            using (var datareader = myCommand.ExecuteReader())
+            {
+                while (datareader.Read())
+                {
+                    User item = new User();
+                    user.userID = Convert.ToInt32(datareader["UserID"]);
+                    user.firstname = $"{datareader["FirstName"]}";
+                    user.lastname = $"{datareader["LastName"]}";
+                    user.username = $"{datareader["Username"]}";
+                    user.streetAdress = $"{datareader["StreetAdress"]}";
+                    user.zipcodeID = Convert.ToInt32(datareader["ZipcodeID"]);
+                    user.cityID = Convert.ToInt32(datareader["CityID"]);
+                    user.customergroupID = Convert.ToInt32(datareader["CustomergroupID"]);
+                    user.admin = Convert.ToBoolean(datareader["Admin"]);
+                    users.Add(item);
+                }
+            }
+            return users;
+        }
+
+        public void UpdateUser (int user)
+        {
+            SqlCommand cmdUpdate = new SqlCommand("UPDATE tblUser SET FirstName = @newFirstName, LastName = @newLastName, Username = @newUsername, Password = @newPsw, StreetAdress = @newStreetAddress WHERE UserID = @UserID", connection);
+
+            cmdUpdate.Parameters.AddWithValue("@UserID", user);
+
+
+        }
+
+        public void InsertUser(User user)
+        {
+            SqlCommand insertUser = new SqlCommand("INSERT INTO tblUser (FirstName, LastName, Username, Password, StreetAdress) VALUES(@newFirstName, @newLastName, @newUsername, @newPsw, @newStreetAddress");
+            insertUser.Parameters.AddWithValue("@newFirstName", user.firstname);
+            insertUser.Parameters.AddWithValue("@newLastName", user.lastname);
+            insertUser.Parameters.AddWithValue("@newUsername", user.username);
+            insertUser.Parameters.AddWithValue("@newPsw", user.password);
+            insertUser.Parameters.AddWithValue("@newStreetAddress", user.streetAdress);
+            insertUser.ExecuteNonQuery();
+
+        }
+
     }
 }
+
---
+++ Webshop/Webshop/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webshop.Models
+{
+    public class User
+    {
+
+        public int userID { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string streetAdress { get; set; }
+
+
+        public int zipcodeID { get; set; }
+        public int cityID { get; set; }
+        public int customergroupID { get; set; }
+        public bool admin { get; set; }
+         
+    }
+}
