3b9eb239c2a9b318711f4167b2927cd8a0eb74d4
KevinKarlberg <Kevinkarlberg@hotmail.se>
2016-09-30 11:05:53 +0200

    I solved webshop, np

---
+++ Webshop/TestWebshop/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TestWebshop.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Webshop/TestWebshop/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebshop.Startup))]
+namespace TestWebshop
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
